f6ebcf83c073a45198e6fb54a50428c82861f2c5
AlanDeng7 <alan.rb.deng@gmail.com>
2023-04-11 13:31:50 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Module-14-Interactive_Visualizations
+# Project Title
+Module-14-Interactive_Visualizations
+
+## Project Description
+The module 14 challenge will Javascript and HTML to learn to build an interactive dashboard. 
+
+## About
+The files uses in this challenge will in in JavaScript and HTML format <br />
+
+## Getting Started
+Link to dashboard: 
+
+## Contributing
+Alan Deng
+
+## Resources
+Stack Overflow, W3school
--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,11 +3,89 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 const dataPromise = d3.json(url);
 console.log("data", dataPromise);
 
+
+//getting data for the selected value of the drop down menu 
+function optionChanged(selectedID) {
+    d3.json(url).then(function(data){
+        let nameID = Object.values(data.names);
+        let dropdownID = d3.select("#selDataset");
+        currentID = nameID.indexOf(selectedID);
+
+        console.log("ID:"+selectedID + " Array Position:"+currentID);
+
+        //update meta info 
+        let MD_id = d3.select(".panel-body").text("div");
+        MD_id.text("id: " + data.metadata[currentID].id);
+    
+        let MD_ethnicity = d3.select(".panel-body").append("div");
+        MD_ethnicity.text("ethnicity: " + data.metadata[currentID].ethnicity);
+
+        let MD_gender = d3.select(".panel-body").append("div");
+        MD_gender.text("gender: " + data.metadata[currentID].gender);
+
+        let MD_age = d3.select(".panel-body").append("div");
+        MD_age.text("age: " + data.metadata[currentID].age);
+
+        let MD_location = d3.select(".panel-body").append("div");
+        MD_location.text("location: " + data.metadata[currentID].location);
+
+        let MD_bbtype = d3.select(".panel-body").append("div");
+        MD_bbtype.text("bbtype: " + data.metadata[currentID].bbtype);
+
+        let MD_wfreq = d3.select(".panel-body").append("div");
+        MD_wfreq.text("wfreq: " + data.metadata[currentID].wfreq);
+
+
+        //update bar chart
+        let dataBar = {
+            x: data.samples[currentID].sample_values.slice(0, 10).reverse(),
+            y: data.samples[currentID].otu_ids.map(id =>("OTU ") +id).slice(0, 10).reverse(),
+    
+            type: 'bar',
+            orientation: 'h',
+            text: data.samples[currentID].otu_labels.slice(0, 10).reverse(),
+        };
+        Plotly.newPlot("bar", [dataBar]);
+
+        //update bubble chart
+        var dataBubble = {
+            x: data.samples[currentID].otu_ids.slice(0,50),
+            y: data.samples[currentID].sample_values.slice(0, 50),
+            text:data.samples[currentID].otu_labels.slice(0, 50),
+            labelString: "OTU ID",
+            mode: 'markers',
+            marker: {
+                size: data.samples[currentID].sample_values.slice(0, 50),
+                color: data.samples[currentID].otu_ids.slice(0, 50)
+            }
+          };
+
+        Plotly.newPlot("bubble", [dataBubble]);
+
+        //update gauge chart
+        var dataGauge = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: data.metadata[currentID].wfreq,
+                title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs per Week" },
+                type: "indicator",
+                mode: "gauge+number",
+                delta: { reference: 400 },
+                gauge: { axis: { range: [null, 10] } }
+            }
+        ];
+        
+        var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+        Plotly.newPlot('gauge', dataGauge, layout);
+    })
+};
+
+//initial charts 
 d3.json(url).then(function(data){
     console.log(data);
-    
+
     //default horizontal bar chart
-    let trace1 = {
+    let dataBar = {
         x: data.samples[0].sample_values.slice(0, 10).reverse(),
         y: data.samples[0].otu_ids.map(id =>("OTU ") +id).slice(0, 10).reverse(),
 
@@ -15,10 +93,10 @@ d3.json(url).then(function(data){
         orientation: 'h',
         text: data.samples[0].otu_labels.slice(0, 10).reverse(),
     };
-    Plotly.newPlot("bar", [trace1]);
+    Plotly.newPlot("bar", [dataBar]);
 
     //default bubble charthes here
-    var trace2 = {
+    var dataBubble = {
         x: data.samples[0].otu_ids.slice(0,50),
         y: data.samples[0].sample_values.slice(0, 50),
         text:data.samples[0].otu_labels.slice(0, 50),
@@ -31,7 +109,7 @@ d3.json(url).then(function(data){
         }
       };
 
-    Plotly.newPlot("bubble", [trace2]);
+    Plotly.newPlot("bubble", [dataBubble]);
     
     //default meta data info
     let MD_id = d3.select(".panel-body").append("div");
@@ -56,7 +134,7 @@ d3.json(url).then(function(data){
     MD_wfreq.text("wfreq: " + data.metadata[0].wfreq);
 
     //default gauge chart
-    var data = [
+    var dataGauge = [
         {
             domain: { x: [0, 1], y: [0, 1] },
             value: data.metadata[0].wfreq,
@@ -69,14 +147,26 @@ d3.json(url).then(function(data){
     ];
     
     var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
-    Plotly.newPlot('gauge', data, layout);
+    Plotly.newPlot('gauge', dataGauge, layout);
 
-    //default meta data info 
 
+    //list of name ID
+    let nameID = Object.values(data.names);
+    console.log(nameID);
+
+    //adding all name ID to drop down using a for loop
+    let select = document.getElementById("selDataset");
     
-
+    for(let i = 0; i < nameID.length; i++) {
+        let option = document.createElement("option");
+        option.value = nameID[i];
+        option.text = nameID[i];
+        select.add(option);
+    }
 
 });
+
+
 
 
 
